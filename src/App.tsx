e7f08adf84ce023fbe0ea45fd103a3c2ffa7ea27
Anna Panferova <anyapanf@gmail.com>
2024-08-07 13:05:04 +0600

    React-hook-form added

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,29 @@
 import AppCSS from './App.module.css';
+import { useForm, SubmitHandler } from "react-hook-form";
 import { useState } from 'react';
+import FacebookImg from './assets/facebook.svg';
+import LinkedInImg from './assets/linkedin.svg';
+import GoogleImg from './assets/google.svg';
+
+interface IFormInput {
+  name?: string
+  email: string
+  password: string
+}
 
 const App = () => {
   const [rightPanelActive, setRightPanelActive] = useState<boolean>(false);
+  const { register, handleSubmit, reset } = useForm<IFormInput>();
+
+  const onSignUp: SubmitHandler<IFormInput> = (data) => {
+    console.log(data);
+    reset();
+  };
+
+  const onSignIn: SubmitHandler<IFormInput> = (data) => {
+    console.log(data);
+    reset();
+  };
 
   const handleSwitch = () => {
     setRightPanelActive(!rightPanelActive)
@@ -12,32 +33,52 @@ const App = () => {
     <>
       <div className={rightPanelActive ? `${AppCSS.container} ${AppCSS.rightPanelActive}` : `${AppCSS.container}`}>
         <div className={`${AppCSS.formContainer} ${AppCSS.signUpContainer}`}>
-          <form className={AppCSS.form} action="#">
+          <form className={AppCSS.form} onSubmit={handleSubmit(onSignIn)}>
             <h1 className={AppCSS.title}>Sign in</h1>
-            <div className="social-container">
-              <a href="#"><i className="fab fa-facebook-f"></i></a>
-              <a href="#"><i className="fab fa-google-plus-g"></i></a>
-              <a href="#"><i className="fab fa-linkedin-in"></i></a>
+            <div className={AppCSS.socialContainer}>
+              <a href="#"><img src={FacebookImg} alt="Facebook" /></a>
+              <a href="#"><img src={LinkedInImg} alt="LinkedIn" /></a>
+              <a href="#"><img src={GoogleImg} alt="Google+" /></a>
             </div>
             <span>or use your account</span>
-            <input className={AppCSS.formInput} type="email" placeholder="Email" />
-            <input className={AppCSS.formInput} type="password" placeholder="Password" />
+            <input {...register("email")}
+              className={AppCSS.formInput}
+              type="email"
+              placeholder="Email"
+              required />
+            <input {...register("password")}
+              className={AppCSS.formInput}
+              type="password"
+              placeholder="Password"
+              required />
             <a href="#">Forgot your password?</a>
             <button>Sign In</button>
           </form>
         </div>
         <div className={`${AppCSS.formContainer} ${AppCSS.signInContainer}`}>
-          <form action="#" className={AppCSS.form}>
+          <form className={AppCSS.form} onSubmit={handleSubmit(onSignUp)}>
             <h1 className={AppCSS.title}>Create Account</h1>
-            <div className="social-container">
-              <a href="#"><i className="fab fa-facebook-f"></i></a>
-              <a href="#"><i className="fab fa-google-plus-g"></i></a>
-              <a href="#"><i className="fab fa-linkedin-in"></i></a>
+            <div className={AppCSS.socialContainer}>
+              <a href="#"><img src={FacebookImg} alt="Facebook" /></a>
+              <a href="#"><img src={LinkedInImg} alt="LinkedIn" /></a>
+              <a href="#"><img src={GoogleImg} alt="Google+" /></a>
             </div>
             <span>or use your email for registration</span>
-            <input className={AppCSS.formInput} type="text" placeholder="Name" />
-            <input className={AppCSS.formInput} type="email" placeholder="Email" />
-            <input className={AppCSS.formInput} type="password" placeholder="Password" />
+            <input {...register("name")}
+              className={AppCSS.formInput}
+              type="text"
+              placeholder="Name"
+              required />
+            <input {...register("email")}
+              className={AppCSS.formInput}
+              type="email"
+              placeholder="Email"
+              required />
+            <input {...register("password")}
+              className={AppCSS.formInput}
+              type="password"
+              placeholder="Password"
+              required />
             <button>Sign Up</button>
           </form>
         </div>
